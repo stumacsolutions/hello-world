42f8fa2508ceba2f6555deccbbd92fdef888539c
Stuart McGregor <stumacsolutions@gmail.com>
2016-08-31 22:17:27 +0100

    Replacing spring boot test with plain unit test.

---
+++ src/test/java/hello/ApplicationTest.java
@@ -0,0 +1,21 @@
+package hello;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ApplicationTest
+{
+    private Application application;
+
+    @Before
+    public void setUp() {
+        application = new Application();
+    }
+
+    @Test
+    public void shouldSayHelloWhenHomeIsAccessed() {
+        assertEquals("Hello, World!", application.home());
+    }
+}
