dc1dacaad8431e5eedc149d9f8d299f2b6342419
Stuart McGregor <stumacsolutions@gmail.com>
2016-08-31 21:21:03 +0100

    Splitting ApplicationRunner file out of Application file.

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,6 +1,5 @@
 package hello;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +12,5 @@ public class Application
     public String home()
     {
         return "Hello World!";
-    }
-
-    public static void main(String[] args)
-    {
-        SpringApplication.run(Application.class, args);
     }
 }
---
+++ src/main/java/hello/ApplicationRunner.java
@@ -0,0 +1,11 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+
+public class ApplicationRunner
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class, args);
+    }
+}
